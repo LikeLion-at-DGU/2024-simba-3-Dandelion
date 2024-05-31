b3d13e4e724ff7a85acf041242cf59c66a8e2da8
mingi <mintim01@naver.com>
2024-06-01 00:29:13 +0900

    feat: 카테고리 추가

--- project/accounts/views.py
+++ project/accounts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import auth
+from django.contrib.auth.models import User
 
 # Create your views here.
+def signup(request):
+    if request.method == 'POST' :
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(request, username=username, password=password)
+
+        if user is None:
+            user = User.objects.create_user(
+            username=request.POST['username'],
+            password=request.POST['password']
+        )
+        user.save()
+
+        auth.login(request,user)
+        return redirect("main:mainpage")
+    
+    return render(request, 'accounts/signup.html')
