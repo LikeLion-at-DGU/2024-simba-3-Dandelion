c8c0605cb84107a3c8168585be69c0020f55e748
forestsoob <vini20022203@gmail.com>
2024-06-17 05:25:39 +0900

    feat: 과거, 미래 함수작성

--- project/post/urls.py
+++ project/post/urls.py
@@ -4,5 +4,7 @@ from .views import *
 app_name = "post"
 urlpatterns = [
     path('past/', past, name="past"),
-     path('past_result/', past_result, name="past_result"),
+    path('past_result/', past_result, name="past_result"),
+    path('future/', future, name="future"),
+    path('future_result/', future_result, name="future_result"),
 ]
--- project/post/views.py
+++ project/post/views.py
@@ -23,7 +23,25 @@ def past_result(request):
     past.category = Category.objects.get(id=1) # 참회로 고정
     past.save()
     return render(request, 'post/past_result.html', {'past' : past})
-   
+
         
+def future(request):
+    if request.user.is_authenticated:
+        return render(request, 'post/future.html')
+    else:
+        return redirect('accounts:login')
+
 def future_result(request):
-    pass #잠깐 비워둠
+    
+    future = future()
+
+    future.text = request.POST["user_text"]
+    sutras = Sutra.objects.filter(category=2)
+    if sutras.exists():
+        sutra = random.choice(sutras)
+        future.my_sutra = sutra
+    future.user = request.user
+    
+    future.category = Category.objects.get(id=2) # 소망으로 고정
+    future.save()
+    return render(request, 'post/future_result.html', {'future' : future})
