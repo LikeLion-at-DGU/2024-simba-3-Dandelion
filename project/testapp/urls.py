a9d1e7804f0dc011345738a787c3a61860663764
kimyeonjinn <kkkl8187@naver.com>
2024-06-19 20:51:33 +0900

    feat:page7 수정

--- project/testapp/urls.py
+++ project/testapp/urls.py
@@ -8,9 +8,15 @@ from .views import *
 app_name = "testapp"
 urlpatterns = [
     path('past/', past, name="past"),
+    path('page2/', page2, name="page2"),
+    path('page5/', page5, name="page5"),
+    path('page6/', page6, name="page6"),
+     path('page7/', page7, name="page7"),
 ]
 
-
+# 1. 이것을 직접 입력해야 링크로 연결된다는 의미
+# 2. views.py에 있는 함수의 이름
+# 3. 보통은 모두 같은 이름으로 통일... ㅎㅎ 어디에 쓰이는지는 잘 모름 ><
 
 # mypage/ => 내가 임의로 정하는 것, 보통 식별이 가능하게 여기서는 [past] 기본 url 
 # 두번째: 함수 이름을 써주면 됨, past
