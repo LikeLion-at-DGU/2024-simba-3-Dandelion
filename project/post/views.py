f1f94c5d4b9ffc9cbeedb0d13d8e82b284b0cacb
mingi <mintim01@naver.com>
2024-06-19 15:43:47 +0900

    merge: KMK, LSB

--- project/post/views.py
+++ project/post/views.py
@@ -24,7 +24,50 @@ def past_result(request):
     past.save()
     return render(request, 'post/past_result.html', {'past' : past})
 
-        
+def start_108(request):
+    return render(request, 'post/start_108.html')
+
+def init_108(request):
+    
+    sharedwish = SharedWish.objects.create(user=request.user)
+    sharedwish.num108_count = 0 # 0으로 초기화
+    sharedwish.save()
+
+    context = {
+        'count' : sharedwish.num108_count
+    }
+    return render(request, 'post/do_108.html', context)
+
+def do_108(request):
+
+    sharedwish = SharedWish.objects.filter(user=request.user, text="").first()
+    sharedwish.num108_count += 1
+    sharedwish.save()
+    if sharedwish.num108_count >= 108:
+         return render(request, 'post/result_108.html')
+    else:
+        context = {
+            'count' : sharedwish.num108_count
+        }
+        return render(request, 'post/do_108.html', context)
+
+def post_108(request):
+    if request.method == 'POST':
+        sharedwish = SharedWish.objects.filter(user=request.user, text="").first()
+        sharedwish.text = request.POST["text"]
+        sharedwish.save()
+
+    return render(request, 'post/community.html') # 글 작성 후 커뮤니티 페이지로 이동
+
+def community_108(request):
+    return render(request, 'post/community.html')
+
+def talisman(request): # POST 에 카테고리를 함께 전달
+    if request.method == 'POST':
+        pass
+    else:
+        return render(request, 'post/make_talisman.html')
+
 def future(request):
     if request.user.is_authenticated:
         return render(request, 'post/future.html')
