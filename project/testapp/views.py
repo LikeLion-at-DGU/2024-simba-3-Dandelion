eb435ff16f30349d4492738eebefa1f2e56c0c3c
gn00py48 <gn00py48@gmail.com>
2024-06-19 21:52:38 +0900

    fix:html css 수정

--- project/testapp/views.py
+++ project/testapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def past (request):
-    return render(request, 'main/future_page.html')
+    return render(request, 'main/future.html')
 
 
 
