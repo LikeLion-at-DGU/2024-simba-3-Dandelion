4b1ee1dc31541e63e83a093d5ce00cb09d104543
kimyeonjinn <kkkl8187@naver.com>
2024-06-21 22:17:55 +0900

    feat: past,start_108, talisman(진행중) html  추가 커밋

---
+++ project/testapp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def test (request):
+    return render(request, 'post/talisman_ing.html')
+
+# past가 함수의 이름이 되게 됨, urls.py에서 두 번째에 past라고 써주면 됨. main/frame8.html에서 main/은 main이라는 큰 범위 안에서 접근하겠다는 의미임.
